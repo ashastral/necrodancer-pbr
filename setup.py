5bcf55c8912737a9634db2828c3860d814630aad
Fraxtil <github@xtil.net>
2017-10-08 14:57:47 -0700

    Setup files + rename pbr-cli to pbr_cli

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='necrodancer-pbr',
+    version='0.1',
+    description='Pre-Built Racing mod generator for Crypt of the NecroDancer',
+    url='https://github.com/fraxtil/necrodancer-pbr',
+    author='Fraxtil',
+    packages=['pbr'],
+    include_package_data=True,
+    entry_points={
+        'console_scripts': [
+            'pbr-cli=pbr_cli:main'
+        ]
+    }
+)
